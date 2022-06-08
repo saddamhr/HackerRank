f37f7d45845f85cd6ae3087887693ace1b1e35ac
saddamhr <saddambubt65@gmail.com>
2022-06-08 12:37:49 +0600

    time conversion solved

--- time-conversion.py
+++ time-conversion.py
@@ -1,10 +1,18 @@
-#
 
 def timeConversion(s):
-    print(s[8])
-    # Write your code here
+    hour = int(s[:2])
+    am_or_pm = s[8:]
+    if am_or_pm == 'PM':
+        if hour != 12:
+            hour = str(hour + 12)
+    if am_or_pm == 'AM':
+        if hour == 12:
+            hour = 0
+    hour = (str(hour).zfill(2))
+    return f'{hour}{s[2:-2]}'
+
 
 if __name__ == '__main__':
 
     s = input()
-    timeConversion(s)
+    print(timeConversion(s))
